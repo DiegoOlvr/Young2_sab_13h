cba52d95b14931997b7612e921fa4d94fac7205f
Diego Oliveira Ramos <oliver.diegoramos@gmail.com>
2024-10-19 15:32:49 -0300

    Aula 11

--- Aula 10/script.js
+++ Aula 10/script.js
@@ -35,5 +35,80 @@ const BOTAO = document.querySelector('button')
 const QUIZ_CONTAINER = document.querySelector('.container')
 const ALTERNATIVAS = document.querySelectorAll('.resposta')
 
+let verificou = false 
+let acerto = 0
+let questao_atual = 0
+
 // PASSO 2 - CRIAR EVENTOS
+window.onload = carregar_quiz()
+BOTAO.addEventListener('click', verificar)
+
 // PASSO 3 - CRIAR AS FUNÇÕES DOS EVENTOS
+function carregar_quiz() {
+    const QUESTAO = QUESTOS_QUIZ[questao_atual]
+
+    ALTERNATIVAS.forEach((alternativa) => {
+        alternativa.checked = false
+        document.querySelector(`label[for=${alternativa.id}]`).classList.remove('correta', 'incorreta')
+    })
+
+    QUIZ_TITULO.innerText = QUESTAO.questao
+    ALTERNATIVA_A.innerText = QUESTAO.a
+    ALTERNATIVA_B.innerText = QUESTAO.b
+    ALTERNATIVA_C.innerText = QUESTAO.c
+    ALTERNATIVA_D.innerText = QUESTAO.d
+    BOTAO.innerText = 'Verificar'
+    verificou = false
+}
+
+function verificar() {
+    if (verificou == false) {
+        verificar_resposta()
+    }
+    else {
+        proxima_pergunta()
+    }
+}
+
+function verificar_resposta() {
+    const RESPOSTA_CORRETA = QUESTOS_QUIZ[questao_atual].correta
+    const RESPOSTA_JOGADOR = pegar_resposta()
+
+    ALTERNATIVAS.forEach((alternativa) => {
+        const LABEL = document.querySelector(`label[for=${alternativa.id}]`)
+        if (alternativa.id == RESPOSTA_CORRETA) {
+            LABEL.classList.add('correta')
+        }
+        else{
+            LABEL.classList.add('incorreta')
+        }
+    })
+
+    if (RESPOSTA_CORRETA == RESPOSTA_JOGADOR) {
+        acerto++
+    }
+    BOTAO.innerText = 'Próxima'
+    verificou = true
+}
+
+function pegar_resposta() {
+    let resposta = undefined
+
+    ALTERNATIVAS.forEach((alternativa) => {
+        if (alternativa.checked == true) {
+            resposta = alternativa.id
+        }
+    })
+    return resposta
+}
+
+function proxima_pergunta() {
+    questao_atual++
+    maximo_perguntas = QUESTOS_QUIZ.length
+    if (questao_atual < maximo_perguntas) {
+        carregar_quiz()
+    }
+    else {
+        QUIZ_CONTAINER.innerHTML = `<h1 id="questao"> Você acertou ${acerto} de ${maximo_perguntas} perguntas! </h1>`
+    }
+}
